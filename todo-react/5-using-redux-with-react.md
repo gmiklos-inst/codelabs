27fd1a75285044aeeb7d61b310b21db12cfe17f9
Gabriel Miklos <gmiklos@instructure.com>
2019-04-29 10:36:58 +0200

    Docs progress - chapter 5

--- todo-react/5-using-redux-with-react.md
+++ todo-react/5-using-redux-with-react.md
@@ -5,3 +5,28 @@ nav_order: 5
 ---
 
 # Using Redux with React
+
+Now that we have our actions, reducer and store in place we can begin integrating our components with Redux. Start by installing the React bindings for Redux:
+
+```
+$ npm i --save react-redux
+```
+
+# Connecting the application to the store
+
+In order to make the Redux store available to all our components the following two changes need to be made in `index.tsx`:
+
+* Import the application store and the `<Provider/>` tag: 
+```typescript jsx
+import {createAppStore} from "./store";
+import { Provider } from 'react-redux';
+```
+
+* Wrap the application component with the provider tag:
+```typescript jsx
+ReactDOM.render(<Provider store={createAppStore()}><App /></Provider>, container);
+```
+
+The `<Provider />` tag makes the store available to all the components that had been wrapped using the `connect()` function - connecting components is how we make them React-aware.
+
+While you can connect and wrap an arbitrary number of components for the purposes of our simple application we are only going to wrap the main `<App/>` component which will then propagate any changes through properties.
--- todo-react/chkpoint6-implement-redux/index.tsx
+++ todo-react/chkpoint6-implement-redux/index.tsx
@@ -10,43 +10,13 @@ import '@inst/bridge-ui-components.icon-button/dist/main.css'
 import '@inst/bridge-ui-components.icon/dist/main.css'
 import '@inst/bridge-ui-components.tooltip/dist/main.css'
 
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import React from 'react';
+
+import { App } from "./app";
+import {createPersistedAppStore} from "./store";
 import { Provider } from 'react-redux';
-import { appReducer, AppState } from './reducers';
-import { App } from './app';
 
-export const loadState = (): AppState | undefined => {
-    try {
-      const serializedState = localStorage.getItem('state');
-      if (serializedState === null) {
-        return undefined;
-      }
-      return JSON.parse(serializedState);
-    } catch (err) {
-      return undefined;
-    }
-  }; 
-  
-  export const saveState = (state: AppState) => {
-    try {
-      const serializedState = JSON.stringify(state);
-      localStorage.setItem('state', serializedState);
-    } catch {
-      // ignore errors
-    }
-  };
-  
-  const store = createStore(appReducer, loadState());
-  
-  store.subscribe(() => {
-    saveState(store.getState());
-  });
-  
-  
-  const container = document.getElementById('app-container');
+const container = document.getElementById('app-container');
 
-
-  ReactDOM.render(<Provider store={store}><App /></Provider>, container);
-  
+ReactDOM.render(<Provider store={createPersistedAppStore()}><App /></Provider>, container);
